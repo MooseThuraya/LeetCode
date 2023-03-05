0b9425760b74b71bf8c64fdb918341652ac1701d
Mustafa Abuthuraya <mostafufu@hotmail.com>
2023-03-04 19:42:09 -0800

    Adding solved problem

--- 67-add-binary.py
+++ 67-add-binary.py
@@ -19,3 +19,21 @@ class Solution(object):
         if carry:
             res = "1" + res
         return res
+    
+
+        # More efficient n space (not quadratic)
+        res = [""]
+        carry = 0
+        a, b = a[::-1], b[::-1]
+        for i in range(max(len(a), len(b))):
+            digitA = int(a[i]) if i < len(a) else 0
+            digitB = int(b[i]) if i < len(b) else 0
+
+            total = digitA + digitB + carry
+            char = str(total%2)
+            res.append(char)
+            carry = total//2
+        if carry:
+            res.append("1")
+        res.reverse()
+        return "".join(res)
