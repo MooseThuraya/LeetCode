ea35fa8d32f6d5a96fa8e1849d6e1e0b4c73933e
moosethuraya <mostafufu@hotmail.com>
2023-09-20 19:27:25 -0700

    Solved Hand of Straights

---
+++ 846-hand-of-straights.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        
+        count = {}
+        for h in hand:
+            if count.get(h) is None:
+                count[h] = 1
+            else:
+                count[h]+=1
+        minH = list(count)
+        heapq.heapify(minH)
+        while len(minH) > 0:
+            start = minH[0]
+            for i in range(start, start + groupSize):
+                if i not in count:
+                    return False
+                count[i]-=1
+                if count[i] == 0:
+                    if i != minH[0]:
+                        return False
+                    heapq.heappop(minH)
+        return True
+
+
+        
+
+        
